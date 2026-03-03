a965a3c82596ef806827543c251c3c9c6bfcb3e3
DaniRZ10 <drioszea@gmail.com>
2026-03-03 11:12:54 +0100

    Implemento atributos, métodos, constructores, getters y setters #7

--- Videojuego/src/modelo/Guerrero.java
+++ Videojuego/src/modelo/Guerrero.java
@@ -2,4 +2,18 @@ package modelo;
 
 public class Guerrero extends Personaje {
 
+    public Guerrero(String nombre, int vida, int ataque, int defensa, double critico, int velocidad) {
+        super(nombre, vida, ataque, defensa, critico, velocidad);
+    }
+
+    @Override
+    public void atacar() {
+
+    }
+
+    @Override
+    public void defenderse() {
+
+    }
+
 }
--- Videojuego/src/modelo/Personaje.java
+++ Videojuego/src/modelo/Personaje.java
@@ -1,20 +1,152 @@
 package modelo;
 
 public abstract class Personaje {
+    // 1. Atributos de Identidad y Combate
+    protected String nombre;
     protected int vida;
-    protected int daño;
+    protected int ataque;
     protected int defensa;
     protected double critico;
 
-    public void atacar() {
+    // 2. Atributos de Movimiento
+    protected int posX;
+    protected int posY;
+    protected int velocidad;
+
+    // Constructor base
+    public Personaje(String nombre, int vida, int ataque, int defensa, double critico, int velocidad) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.critico = critico;
+        this.velocidad = velocidad;
+
+        // Valores iniciales por defecto
+        this.posX = 0;
+        this.posY = 0;
     }
 
-    public void defensa() {
+    // --- MÉTODOS DE MOVIMIENTO ---
 
+    // Moverse en una dirección
+
+    public void moverDireccion(String direccion) {
+        switch (direccion.toLowerCase()) {
+            case "arriba":
+                this.posY = this.posY + velocidad;
+                break;
+            case "abajo":
+                this.posY = this.posY - velocidad;
+                break;
+            case "izquierda":
+                this.posX = this.posX + velocidad;
+                break;
+            case "derecha":
+                this.posX = this.posX - velocidad;
+                break;
+
+            default:
+                System.err.println("Dirección no válida");
+                return;
+        }
+
+    }
+
+    // --- MÉTODOS DE COMBATE ---
+
+    // Hacemos atacar() abstracto porque cada clase heredada atacará distinto.
+
+    public abstract void atacar();
+
+    // Lo mismo con la defensa o habilidades especiales
+    public abstract void defenderse();
+
+    // Métodos comunes para todos los personajes
+
+    public void recibirDaño(int cantidad) {
+
+        // La defensa reduce el daño recibido
+
+        int danioReal = Math.max(0, cantidad - this.defensa);
+        this.vida = this.vida - danioReal;
+
+        System.out.println(nombre + " recibe " + danioReal + " puntos de daño.");
+
+        if (this.vida <= 0) {
+            this.vida = 0;
+            System.out.println(nombre + " ha sido derrotado...");
+        }
     }
 
     public void usarObjeto() {
+        System.out.println(nombre + " usó un objeto misterioso.");
+    }
 
+    // === GETTERS y SETTERS ===
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public double getCritico() {
+        return critico;
+    }
+
+    public void setCritico(double critico) {
+        this.critico = critico;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
     }
 
 }
